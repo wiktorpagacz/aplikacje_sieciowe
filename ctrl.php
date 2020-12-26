397d7258ec1535e3f839b18b553fe04407fa68f3
Wiktor Pagacz <wiktor.pagacz@gmail.com>
2020-12-26 20:22:48 +0100

    Implement session

--- ctrl.php
+++ ctrl.php
@@ -3,11 +3,22 @@ require_once 'init.php';
 
 switch ($action) {
     default : //calcView
+        include 'check.php';
         $ctrl = new app\controllers\CalcCtrl();
         $ctrl->generateView();
     break;
+    case 'login' :
+        $ctrl = new app\controllers\LoginCtrl();
+        $ctrl->doLogin();
+    break;
     case 'credCalcCompute' :
+        include 'check.php';
         $ctrl = new app\controllers\CalcCtrl();
         $ctrl->process();
+    break;
+    case 'logout' :
+        include 'check.php';
+        $ctrl = new app\controllers\LoginCtrl();
+        $ctrl->doLogout();
     break;
 }
